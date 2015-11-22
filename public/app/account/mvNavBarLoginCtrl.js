a1fb4e515fdf6553cb78591e037c354796ae6f2f
Andrey P <pfm@live.ru>
2015-11-22 17:29:22 +0300

    finish authenticate

---
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -0,0 +1,15 @@
+angular.module("app").controller("mvNavBarLoginCtrl", ['$scope', '$http','mvIdentity', 'mvNotifier', 'mvAuth', function($scope, $http, mvIdentity, mvNotifier, mvAuth){
+
+    $scope.identity = mvIdentity;
+
+    $scope.signin= function(username, password){
+
+        mvAuth.authenticateUser(username, password).then(function(success) {
+            if (success) {
+                mvNotifier.notify('You have successfully signed in!');
+            } else {
+                mvNotifier.notify('Username/password combination incorrect.');
+            }
+        });
+    }
+}]);
